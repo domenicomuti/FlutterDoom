708cb152b0c1f75e354ba5e8cde62ee0cfe5601c
Domenico Muti <noelyoung84@gmail.com>
2025-12-13 11:48:07 +0100

    Physical Keyboard Working

--- lib/keyboard/portrait_keyboard.dart
+++ lib/keyboard/portrait_keyboard.dart
@@ -34,118 +34,129 @@ class _PortraitKeyboardState extends State<PortraitKeyboard> {
   }
 
   KeyEventResult _handleKeyPress(FocusNode node, KeyEvent event) {
-    int asciiCode = 0;
+    int asciiCode;
 
-    bool isSpecialKey = event.logicalKey.keyId & 0xF00000000 > 0;
+    switch (event.logicalKey.keyLabel) {
+      case "Arrow Right":
+        asciiCode = AsciiKeys.keyCodes["KEY_RIGHTARROW"]!;
+        break;
 
-    if (!isSpecialKey) {
-      print("DEFAULT");
-      asciiCode = event.logicalKey.keyId;
+      case "Arrow Left":
+        asciiCode = AsciiKeys.keyCodes["KEY_LEFTARROW"]!;
+        break;
+
+      case "Arrow Up":
+        asciiCode = AsciiKeys.keyCodes["KEY_UPARROW"]!;
+        break;
+
+      case "Arrow Down":
+        asciiCode = AsciiKeys.keyCodes["KEY_DOWNARROW"]!;
+        break;
+
+      case "Escape":
+      case "Go Back":
+        asciiCode = AsciiKeys.keyCodes["KEY_ESCAPE"]!;
+        break;
+
+      case "Enter":
+      case "Numpad Enter":
+        asciiCode = AsciiKeys.keyCodes["KEY_ENTER"]!;
+        break;
+
+      case "Tab":
+        asciiCode = AsciiKeys.keyCodes["KEY_TAB"]!;
+        break;
+
+      case "Control Left":
+      case "Control Right":
+        asciiCode = AsciiKeys.keyCodes["KEY_RCTRL"]!;
+        break;
+
+      case "Shift Right":
+      case "Shift Left":
+        asciiCode = AsciiKeys.keyCodes["KEY_RSHIFT"]!;
+        break;
+
+      case "Backspace":
+        asciiCode = AsciiKeys.keyCodes["KEY_BACKSPACE"]!;
+        break;
+
+      case "F1":
+        asciiCode = AsciiKeys.keyCodes["KEY_F1"]!;
+        break;
+
+      case "F2":
+        asciiCode = AsciiKeys.keyCodes["KEY_F2"]!;
+        break;
+
+      case "F3":
+        asciiCode = AsciiKeys.keyCodes["KEY_F3"]!;
+        break;
+
+      case "F4":
+        asciiCode = AsciiKeys.keyCodes["KEY_F4"]!;
+        break;
+
+      case "F5":
+        asciiCode = AsciiKeys.keyCodes["KEY_F5"]!;
+        break;
+
+      case "F6":
+        asciiCode = AsciiKeys.keyCodes["KEY_F6"]!;
+        break;
+
+      case "F7":
+        asciiCode = AsciiKeys.keyCodes["KEY_F7"]!;
+        break;
+
+      case "F8":
+        asciiCode = AsciiKeys.keyCodes["KEY_F8"]!;
+        break;
+
+      case "F9":
+        asciiCode = AsciiKeys.keyCodes["KEY_F9"]!;
+        break;
+
+      case "F10":
+        asciiCode = AsciiKeys.keyCodes["KEY_F10"]!;
+        break;
+
+      case "F11":
+        asciiCode = AsciiKeys.keyCodes["KEY_F11"]!;
+        break;
+
+      case "F12":
+        asciiCode = AsciiKeys.keyCodes["KEY_F12"]!;
+        break;
+
+      case "Pause":
+        asciiCode = AsciiKeys.keyCodes["KEY_PAUSE"]!;
+        break;
+
+      case "Alt Left":
+        asciiCode = AsciiKeys.keyCodes["KEY_LALT"]!;
+        break;
+
+      case "Alt Right":
+        asciiCode = AsciiKeys.keyCodes["KEY_RALT"]!;
+        break;
+
+      case "Z":
+        asciiCode = AsciiKeys.keyCodes[","]!;
+        break;
+
+      case "X":
+        asciiCode = AsciiKeys.keyCodes["."]!;
+        break;
+
+      default:
+        asciiCode = event.logicalKey.keyId;
     }
-    else {
-      print("SPECIAL");
-      switch (event.logicalKey.keyLabel) {
-        case "Arrow Right":
-          asciiCode = AsciiKeys.keyCodes["KEY_RIGHTARROW"]!;
-          break;
 
-        case "Arrow Left":
-          asciiCode = AsciiKeys.keyCodes["KEY_LEFTARROW"]!;
-          break;
-
-        case "Arrow Up":
-          asciiCode = AsciiKeys.keyCodes["KEY_UPARROW"]!;
-          break;
-
-        case "Arrow Down":
-          asciiCode = AsciiKeys.keyCodes["KEY_DOWNARROW"]!;
-          break;
-
-        case "Escape":
-          asciiCode = AsciiKeys.keyCodes["KEY_ESCAPE"]!;
-          break;
-
-        case "Enter":
-        case "Numpad Enter":
-          asciiCode = AsciiKeys.keyCodes["KEY_ENTER"]!;
-          break;
-
-        case "Tab":
-          asciiCode = AsciiKeys.keyCodes["KEY_TAB"]!;
-          break;
-
-        case "Control Right":
-          asciiCode = AsciiKeys.keyCodes["KEY_RCTRL"]!;
-          break;
-
-
-        /*"KEY_F1": 0x80+0x3b,
-        "KEY_F2": 0x80+0x3c,
-        "KEY_F3": 0x80+0x3d,
-        "KEY_F4": 0x80+0x3e,
-        "KEY_F5": 0x80+0x3f,
-        "KEY_F6": 0x80+0x40,
-        "KEY_F7": 0x80+0x41,
-        "KEY_F8": 0x80+0x42,
-        "KEY_F9": 0x80+0x43,
-        "KEY_F10": 0x80+0x44,
-        "KEY_F11": 0x80+0x57,
-        "KEY_F12": 0x80+0x58,
-        "KEY_BACKSPACE": 127,
-        "KEY_PAUSE": 0xff,
-        "KEY_EQUALS": 0x3d,
-        "KEY_MINUS": 0x2d,
-        "KEY_RSHIFT": 0x80+0x36,
-        "KEY_RCTRL": 0x80+0x1d,
-        "KEY_RALT": 0x80+0x38,
-        "KEY_LALT": 0x80+0x38,
-        ",": 44,
-        ".": 46,
-        "SPACE": 32,
-        "1": 49,
-        "2": 50,
-        "3": 51,
-        "4": 52,
-        "5": 53,
-        "6": 54,
-        "7": 55,
-        "Y": 121*/
-      }
-    }
-
-    print(event);
+    //print(event);
 
     engine.dartPostInput(asciiCode, event is KeyDownEvent || event is KeyRepeatEvent ? 1 : 0);
     return KeyEventResult.handled;
-
-
-
-    /*if (event is KeyDownEvent) {
-      debugPrint('Focus node ${node.debugLabel} got key event: ${event.logicalKey}');
-      switch (event.logicalKey) {
-        case LogicalKeyboardKey.keyR:
-          debugPrint('Changing color to red.');
-          setState(() {
-            _color = Colors.red;
-          });
-          return KeyEventResult.handled;
-        case LogicalKeyboardKey.keyG:
-          debugPrint('Changing color to green.');
-          setState(() {
-            _color = Colors.green;
-          });
-          return KeyEventResult.handled;
-        case LogicalKeyboardKey.keyB:
-          debugPrint('Changing color to blue.');
-          setState(() {
-            _color = Colors.blue;
-          });
-          return KeyEventResult.handled;
-      }
-    }*/
-
-    print(event);
-    return KeyEventResult.ignored;
   }
 
   @override
